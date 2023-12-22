cdc754dc589c183d072051f3fa28824ff33e35fb
Michael Rooplall <MichaelRooplall@gmail.com>
2023-12-21 23:05:59 -0500

    Implement includeWhite, hasTransparency, and hasTransparencyConfig for color-thief-node.js

--- src/color-thief-node.js
+++ src/color-thief-node.js
@@ -1,9 +1,14 @@
 const getPixels = require('get-pixels');
 const quantize = require('@lokesh.dhakar/quantize');
 
-function createPixelArray(imgData, pixelCount, quality) {
+function createPixelArray(imgData, pixelCount, options) {
+
+    const { quality, includeWhite, checkTransparency, checkTransparencyConfig } = options;
+
     const pixels = imgData;
     const pixelArray = [];
+
+    let transparentPixels = 0;
 
     for (let i = 0, offset, r, g, b, a; i < pixelCount; i = i + quality) {
         offset = i * 4;
@@ -13,17 +18,23 @@ function createPixelArray(imgData, pixelCount, quality) {
         a = pixels[offset + 3];
 
         // If pixel is mostly opaque and not white
-        if (typeof a === 'undefined' || a >= 125) {
-            if (!(r > 250 && g > 250 && b > 250)) {
+        if (typeof a === 'undefined' || a >= 125 ) {
+            if (!(r > 250 && g > 250 && b > 250) || includeWhite) {
                 pixelArray.push([r, g, b]);
             }
+        } else if ( checkTransparency && a < checkTransparencyConfig.pixelConsideredTransparentThreshold ) {
+            transparentPixels++;
         }
     }
-    return pixelArray;
+
+    const hasTransparency = checkTransparency && ((transparentPixels / pixelCount.length) > checkTransparencyConfig.imageConsideredTransparentThreshold);
+
+    return {pixelArray, hasTransparency}
+
 }
 
 function validateOptions(options) {
-    let { colorCount, quality } = options;
+    let { colorCount, quality, includeWhite, checkTransparency, checkTransparencyConfig } = options;
 
     if (typeof colorCount === 'undefined' || !Number.isInteger(colorCount)) {
         colorCount = 10;
@@ -38,9 +49,46 @@ function validateOptions(options) {
         quality = 10;
     }
 
+    if (typeof includeWhite == 'undefined' || typeof includeWhite !== 'boolean' ){
+        includeWhite = false;
+    }
+
+    if (typeof checkTransparency == 'undefined' || typeof checkTransparency !== 'boolean' ){
+        checkTransparency = false;
+    } else {
+        if (typeof checkTransparencyConfig == 'undefined' || typeof checkTransparencyConfig !== 'object') {
+            checkTransparencyConfig = {
+                pixelConsideredTransparentThreshold : 10,
+                imageConsideredTransparentThreshold : 0.1
+            }
+        } else {
+            const {pixelConsideredTransparentThreshold, imageConsideredTransparentThreshold} = checkTransparencyConfig;
+
+            if (typeof pixelConsideredTransparentThreshold == 'undefined' || Number.isNaN(pixelConsideredTransparentThreshold)) {
+                checkTransparencyConfig.pixelConsideredTransparentThreshold = 10;
+            }
+
+            if (pixelConsideredTransparentThreshold < 0 || pixelConsideredTransparentThreshold > 255) {
+                throw new Error('pixelConsideredTransparentThreshold should be between 0 and 255. Default is 10, meaning if a pixel\'s alpha (0-255) is less than this 10, the pixel is counted towards the overall transparency check.');
+            }
+
+            if (typeof imageConsideredTransparentThreshold == 'undefined' || Number.isNaN(imageConsideredTransparentThreshold)) {
+                checkTransparencyConfig.imageConsideredTransparentThreshold = 0.1;
+            }
+
+            if (imageConsideredTransparentThreshold < 0 || imageConsideredTransparentThreshold > 1) {
+                throw new Error('pixelConsideredTransparentThreshold should be between 0 and 1. Default is 0.1, meaning if 10% of the pixels trigger the pixelConsideredTransparentThreshold check, the image is considered transparent.');
+            }
+
+        }
+    }
+
     return {
         colorCount,
-        quality
+        quality,
+        includeWhite,
+        checkTransparency,
+        checkTransparencyConfig
     }
 }
 
@@ -56,9 +104,9 @@ function loadImg(img) {
     });
 }
 
-function getColor(img, quality) {
+function getColor(img, quality, includeWhite = false) {
     return new Promise((resolve, reject) => {
-        getPalette(img, 5, quality)
+        getPalette(img, 5, quality, includeWhite)
             .then(palette => {
                 resolve(palette[0]);
             })
@@ -69,22 +117,29 @@ function getColor(img, quality) {
 
 }
 
-function getPalette(img, colorCount = 10, quality = 10) {
+function getPalette(img, colorCount = 10, quality = 10, includeWhite = false, checkTransparency = false, checkTransparencyConfig = {pixelConsideredTransparentThreshold : 10, imageConsideredTransparentThreshold : 0.1}) {
     const options = validateOptions({
         colorCount,
-        quality
+        quality,
+        includeWhite,
+        checkTransparency,
+        checkTransparencyConfig
     });
 
     return new Promise((resolve, reject) => {
         loadImg(img)
             .then(imgData => {
                 const pixelCount = imgData.shape[0] * imgData.shape[1];
-                const pixelArray = createPixelArray(imgData.data, pixelCount, options.quality);
+                const {pixelArray, hasTransparency} = createPixelArray(imgData.data, pixelCount, options);
 
                 const cmap = quantize(pixelArray, options.colorCount);
                 const palette = cmap? cmap.palette() : null;
 
-                resolve(palette);
+                if (checkTransparency) {
+                    resolve({palette, hasTransparency})
+                } else {
+                    resolve(palette);
+                }
             })
             .catch(err => {
                 reject(err);
